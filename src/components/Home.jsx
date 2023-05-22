d05f496d52c71c8d5b40566dc82bf02617a70682
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-05-22 18:26:21 -0400

    creacion de formulario

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,29 @@
-import { React } from 'react'
+import { React, useState } from 'react'
 
 export default function Dashboard(){
-    const notes = [
+    const initialNotes = {id: '', title: '', body:''}
+
+    const [notes, setNotes] = useState([
         {id:1, title: 'nota 1', body:'prueba xD'},
         {id:2, title: 'nota 2', body:'prueba xD'},
         {id:3, title: 'nota 3', body:'prueba xD'},
         {id:4, title: 'nota 4', body:'prueba xD'},
         {id:5, title: 'nota 5', body:'prueba xD'},
-    ]
+    ]);
+
+    const [note, setNote] = useState(initialNotes);
+
+    const addNotes = (ev) => {
+        ev.preventDefault();
+        const id = notes.length+1;
+        setNotes([
+            ...notes,
+            {
+                ...note,
+                id: id
+            }
+        ])
+    }
 
     return (
         <>
@@ -15,10 +31,23 @@ export default function Dashboard(){
             <ul>
                 {
                     notes.map(note => {
-                        return <li>Id: {note.id} Titulo: {note.title} Cuerpo: {note.body}</li>
+                        return <li key={note.id}>Id: {note.id} Titulo: {note.title} Cuerpo: {note.body}</li>
                     })
                 } 
             </ul>
+            <form onSubmit={(ev) => addNotes(ev)}>
+                <label htmlFor="title">
+                    Titulo
+                    <input id='title' type="text" onChange={(ev) => setNote({...note, title: ev.target.value})} />
+                </label>
+                <br />
+                <label htmlFor="body">
+                    Cuerpo
+                    <input id='body' type="text" onChange={(ev) => setNote({...note, body: ev.target.value})} />
+                </label>
+                <br />
+                <button>Agregar</button>
+            </form>
            
         </>
         
